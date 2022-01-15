42dda046db99e6bc1b53e11445d6a09b9378ab79
Semyon Kirekov <semyon@kirekov.com>
2022-01-15 15:00:33 +0300

    add authorizeUser

--- src/backend/user.ts
+++ src/backend/user.ts
@@ -5,7 +5,7 @@ export interface UserAuthResponse {
   token: string;
 }
 
-export default function authorizeUser(login: string, password: string) {
+export default function authorizeUser(login: string, password: string): Promise<unknown> {
   return axiosNoAuth.post<UserAuthResponse>('/api/auth', { login, password })
     .then((response) => TokenStorage.setToken(response.data.token));
 }
